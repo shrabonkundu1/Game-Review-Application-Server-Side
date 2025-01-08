1430dc09167332a2753f56da67f06b3d69dc13fb
ShrabonKundu <shrabonkundu@gmail.com>
2025-01-08 17:48:45 +0600

    Review details route created successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,13 @@ async function run() {
         const cursor = reviewCollection.find();
         const result = await cursor.toArray();
         res.send(result)
+    })
+
+    app.get('/reviews/:id', async(req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
     })
 
     app.post('/reviews', async(req,res)=> {
